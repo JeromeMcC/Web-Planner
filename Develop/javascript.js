26f56d706bffd0238f3e78091316ed29ff09c81c
Jerome McClarin <Jerome.Mcclarin@gmail.com>
2022-01-07 10:46:54 -0500

    time clock function created

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,7 +1,30 @@
 $( document ).ready(function() {
-$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+function timeClock(){
+    var currentHour= moment().hours();
+    $(".time-block").each(function(){
+        var currentBlockHour = parseInt($(this).attr("id").split("-")[1])
+        if (currentBlockHour < currentHour){
+            $(this).addClass("past")
+     }
+     else if (currentBlockHour === currentHour){
+         $(this).addClass("present")
+     }
+
+     else {
+         $(this).addClass("future")
+     }
+    })
+
+    }
+
+    timeClock()
+
 $( ".saveBtn" ).click(function() {
-    alert( "Handler for .click() called." );
+    );
   });
     console.log( "ready!" );
 });
