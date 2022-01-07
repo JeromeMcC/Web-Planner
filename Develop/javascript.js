5578facd239a5ce773f925c3410430850120faac
Jerome McClarin <Jerome.Mcclarin@gmail.com>
2022-01-07 15:28:31 -0500

    finished schedule function

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,35 +1,75 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 
-function timeClock(){
-    var currentHour= moment().hours();
-    $(".time-block").each(function(){
-        var currentBlockHour = parseInt($(this).attr("id").split("-")[1])
-        if (currentBlockHour < currentHour){
-            $(this).addClass("past")
-     }
-     else if (currentBlockHour === currentHour){
-         $(this).addClass("present")
-     }
+    function timeClock() {
+        var currentHour = moment().hours();
+        $(".time-block").each(function () {
+            var currentBlockHour = parseInt($(this).attr("id").split("-")[1])
+            if (currentBlockHour < currentHour) {
+                $(this).addClass("past")
+            }
+            else if (currentBlockHour === currentHour) {
+                $(this).addClass("present")
+            }
 
-     else {
-         $(this).addClass("future")
-     }
-    })
+            else {
+                $(this).addClass("future")
+            }
+        })
 
     }
 
     timeClock()
 
-$( ".saveBtn" ).click(function() {
-  var textContent= $(this).prev().val()
-    console.log( $(this).prev().val())
-    localStorage.setItem("description",textContent)
-    //hour
-    //description
-  });
-});
+    $(".saveBtn").click(function () {
+        var textContent = $(this).prev().val()
+        var hourId = $(this).parent().attr("id")
 
-localStorage.getItem("description")
+        var update = JSON.parse(localStorage.getItem("description"))
+
+        var timeSlot = {
+
+        }
+
+        if (update === null) {
+            timeSlot[hourId] = textContent
+
+            localStorage.setItem("description", JSON.stringify(timeSlot))
+        }
+        else {
+            update[hourId] = textContent
+
+            localStorage.setItem("description", JSON.stringify(update));
+        }
+
+
+    });
+})
+function schedule() {
+    var storedInfo = JSON.parse(localStorage.getItem("description"))
+    if ( storedInfo===null) 
+        return
+    
+    var infoBlock = Object.entries(storedInfo)
+    infoBlock.forEach(function (item) {
+        var hour = item[0]
+        var descr = item[1]
+        var timeslotEl = $("#"+item[0])
+        var textAreaEl= timeslotEl.children("textarea")
+
+        textAreaEl.val(descr)
+
+
+    })
+
+
+}
+
+
+
+
+schedule()
+
+
